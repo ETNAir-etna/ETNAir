a4926949da409134e03364f8d1d79ecaee37c342
mael <mael.aguessy@gmail.com>
2024-12-16 19:53:04 +0100

    DTO de User

--- shared/types/Property.ts
+++ shared/types/Property.ts
@@ -1,55 +1,54 @@
-
 import { PropertyType } from "@prisma/client";
-import { Property as PrismaProperty } from '@prisma/client';
+import { Property as PrismaProperty } from "@prisma/client";
 
 export type Property = {
-    ownerId: string
-    title: string
-    description?: string | null
-    propertyType: PropertyType
-    occupancyMax?: number | null
-    totalBedrooms?: number | null
-    totalBathrooms?: number | null
-    area?: number | null
-    pricePerNight: number
-    mainImgUrl: string
-    roomNumber?: number | null
-    floorNumber?: number | null
-    unitNumber?: number | null
-    streetNumber: number
-    streetName: string
-    city: string
-    zip?: string | null
-    country: string
-    latitude?: number | null
-    longitude?: number | null
-    equipments: string[]
-    pictures?: string[]
-}
+  ownerId: string;
+  title: string;
+  description?: string | null;
+  propertyType: PropertyType;
+  occupancyMax?: number | null;
+  totalBedrooms?: number | null;
+  totalBathrooms?: number | null;
+  area?: number | null;
+  pricePerNight: number;
+  mainImgUrl: string;
+  roomNumber?: number | null;
+  floorNumber?: number | null;
+  unitNumber?: number | null;
+  streetNumber: number;
+  streetName: string;
+  city: string;
+  zip?: string | null;
+  country: string;
+  latitude?: number | null;
+  longitude?: number | null;
+  equipments: string[];
+  pictures?: string[];
+};
 
 export const PropertyDTO = (property: PrismaProperty): Property => {
-    return {
-        ownerId: property.ownerId,
-        title: property.title,
-        description: property.description || "",
-        propertyType: property.propertyType,
-        occupancyMax: property.occupancyMax ?? null,
-        totalBedrooms: property.totalBedrooms ?? null,
-        totalBathrooms: property.totalBathrooms ?? null,
-        area: property.area ?? null,
-        pricePerNight: property.pricePerNight,
-        mainImgUrl: property.mainImgUrl,
-        roomNumber: property.roomNumber ?? null,
-        floorNumber: property.floorNumber ?? null,
-        unitNumber: property.unitNumber ?? null,
-        streetNumber: property.streetNumber,
-        streetName: property.streetName,
-        city: property.city,
-        zip: property.zip ?? null,
-        country: property.country,
-        latitude: property.latitude ?? null,
-        longitude: property.longitude ?? null,
-        equipments: property.equipments || [],
-        pictures: property.pictures || []
-    };
-}
+  return {
+    ownerId: property.ownerId,
+    title: property.title,
+    description: property.description || "",
+    propertyType: property.propertyType,
+    occupancyMax: property.occupancyMax ?? null,
+    totalBedrooms: property.totalBedrooms ?? null,
+    totalBathrooms: property.totalBathrooms ?? null,
+    area: property.area ?? null,
+    pricePerNight: property.pricePerNight,
+    mainImgUrl: property.mainImgUrl,
+    roomNumber: property.roomNumber ?? null,
+    floorNumber: property.floorNumber ?? null,
+    unitNumber: property.unitNumber ?? null,
+    streetNumber: property.streetNumber,
+    streetName: property.streetName,
+    city: property.city,
+    zip: property.zip ?? null,
+    country: property.country,
+    latitude: property.latitude ?? null,
+    longitude: property.longitude ?? null,
+    equipments: property.equipments || [],
+    pictures: property.pictures || [],
+  };
+};
--- shared/types/User.ts
+++ shared/types/User.ts
@@ -1,23 +1,42 @@
-
+import { User as PrismaUser } from "@prisma/client";
 
 export type User = {
+  id: string;
+  email: string;
+  password: string;
+  firstName?: string | null;
+  lasrName?: string | null;
+  gender?: string | null;
+  phoneNumber?: string | null;
+  role: string;
+  status: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  guestRating?: number | null;
+  hostRating?: number | null;
+  summary?: string | null;
+  profileImg?: string | null;
+  requestForDelete: boolean;
+  isSuperHost: boolean;
+};
 
-    id: string;
-    email: string;
-    password: string;
-    firstName?: string | null;
-    lasrName?: string | null;
-    gender?: string | null;
-    phoneNumber?: string | null;
-    role: string;
-    status: string[];
-    createdAt: Date;
-    updatedAt: Date;
-    guestRating?: number | null;
-    hostRating?: number | null;
-    summary?: string | null;
-    profileImg?: string | null;
-    requestForDelete: boolean;
-    isSuperHost: boolean;
-        
-}
+export const DTOUser = (User: PrismaUser): User => {
+  return {
+    id: User.id,
+    email: User.email,
+    firstName: User.firstName ?? null,
+    lasrName: User.lasrName ?? null,
+    gender: User.gender ?? null,
+    phoneNumber: User.phoneNumber ?? null,
+    role: User.role,
+    status: User.status[], 
+    createdAt: User.createdAt,
+    updatedAt: User.updatedAt,
+    guestRating: User.guestRating ?? null,
+    hostRating: User.hostRating ?? null,
+    summary: User.summary ?? null,
+    profileImg: User.profileImg ?? null,
+    requestForDelete: User.requestForDelete,
+    isSuperHost: User.isSuperHost,
+  };
+};
