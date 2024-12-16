c9453d4e42a39186bdf7f682f2bb163a781d55e1
mael <mael.aguessy@gmail.com>
2024-12-16 17:11:33 +0100

    J a dit que rien ne s'etait passé mais elle a change l'erreur 200

--- api/src/helpers/sendJsonPromise.helper.ts
+++ api/src/helpers/sendJsonPromise.helper.ts
@@ -1,37 +1,27 @@
-import { Request, Response, NextFunction } from 'express';
-import { Result } from '../interfaces/result';
-var createError = require('http-errors');
+import { Request, Response, NextFunction } from "express";
+import { Result } from "../interfaces/result";
+var createError = require("http-errors");
 
-export const  sendJsonPromise  = (promise: Result, notFoundMessage: string)  =>
+export const sendJsonPromise =
+  (promise: Result, notFoundMessage: string) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = promise;
 
-    async (req: Request, res: Response, next : NextFunction ) => {
-        
-        
-        try {
-            
-            const result =  promise;
+      if (!result) {
+        return next(createError(404, notFoundMessage));
+      }
 
-            
-            if (!result) {
-                return next(createError(404, notFoundMessage));
-            } 
-
-            if (result.action === "redirect" && result.url) {
-                return res.redirect(result.url)
-            }
-            else if (result.action === "data") {
-                if (JSON. stringify(result.data) === '{}' ) {
-                    return res.status(204).json(result);
-                }
-                return res.status(200).json(result);
-            }
-            
-
-            
-            
-        } catch (error) {
-            return (next(error));
+      if (result.action === "redirect" && result.url) {
+        return res.redirect(result.url);
+      } else if (result.action === "data") {
+        if (JSON.stringify(result.data) === "{}") {
+          return res.status(204).json(result);
         }
-    } 
-
-
+        return res.status(200).json(result);
+      }
+      return res.status(200).json(result);
+    } catch (error) {
+      return next(error);
+    }
+  };
