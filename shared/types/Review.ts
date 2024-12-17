479e55e7d77046ce2ce29099120d24f2032926d4
Joey Bervin <bervin.joey@hotmail.com>
2024-12-17 08:16:12 +0100

    feat: Create Review DTO based on Prisma schema
    - Implemented Review DTO to map Prisma Review model to a simplified format.
    - Added fields like fullName, profileImg, reviewType, content, and rating.
    - Handled optional fields such as createdBy, reservationId, propertyId, directedTo, and relations with creator and receiver.

--- shared/types/Review.ts
+++ shared/types/Review.ts
@@ -1,14 +1,34 @@
-export type Review = {
+import { reviewTypes } from "@prisma/client";
+import { Review as PrismaReview } from '@prisma/client';
+
+export type Review = {                    
+    id: string
     createdBy?: string | null
-    fullName : string
+    fullName: string
     profileImg: string
-    reviewType: string
+    reviewType: reviewTypes
     reservationId?: string | null
-    propertyId? : string | null
-    content  : string
+    propertyId?: string | null
+    content: string
     rating: number
     publishedAt: Date
-    updatedAt  : Date
-    guestId: String
-    HostId: String
+    updatedAt: Date
+    directedTo?: string | null
+}
+
+export const ReviewDTO = (review: PrismaReview): Review => {
+    return {
+        id: review.id,
+        createdBy: review.createdBy,
+        fullName: review.fullName,
+        profileImg: review.profileImg,
+        reviewType: review.reviewType,
+        reservationId: review.reservationId,
+        propertyId: review.propertyId,
+        content: review.content,
+        rating: review.rating,
+        publishedAt: review.publishedAt,
+        updatedAt: review.updatedAt,
+        directedTo: review.directedTo,
+    }
 }
