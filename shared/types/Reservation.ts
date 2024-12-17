4cce67b9be3cd0570f7468d138eea885f65e474b
Joey Bervin <bervin.joey@hotmail.com>
2024-12-17 08:06:09 +0100

    feat: Implement Reservation DTO with type definition
    - Created a Reservation DTO to map Prisma Reservation model to a user-friendly format.
    - Added optional fields for status, reservationType, createdAt, and updatedAt.
    - Defined the Reservation type with necessary fields like guestId, hostId, propertyId, and more.

--- shared/types/Reservation.ts
+++ shared/types/Reservation.ts
@@ -1,3 +1,6 @@
+import { reservationStatus, reservationType } from "@prisma/client";
+import { Reservation as PrismaReservation } from '@prisma/client';
+
 export type Reservation = {                    
     guestId: string
     hostId: string
@@ -8,8 +11,25 @@ export type Reservation = {
     checkOut: string
     NumberOfguests: number
     totalPrice: number
-    status: string
-    reservationType: string           
-    createdAt: Date
-    updatedAt: Date
+    status?: reservationStatus
+    reservationType?: reservationType           
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+
+export const ReservationDTO = (reservation: PrismaReservation): Reservation => {
+    return {
+        guestId: reservation.guestId ,
+        hostId: reservation.hostId ,
+        propertyId: reservation.propertyId ,
+        startDate: reservation.startDate ,
+        endDate  : reservation.endDate ,
+        checkIn: reservation.checkIn ,
+        checkOut: reservation.checkOut ,
+        NumberOfguests: reservation.NumberOfguests ,
+        totalPrice: reservation.totalPrice ,
+        status: reservation.status ,
+        reservationType: reservation.reservationType ,   
+    }
 }
