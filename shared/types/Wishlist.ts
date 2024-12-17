faffb86bcd914fbb80ea01f5fd9ae6326d12241a
Joey Bervin <bervin.joey@hotmail.com>
2024-12-17 08:19:19 +0100

    feat: Create Wishlist DTO based on Prisma schema
    - Implemented Wishlist DTO to map Prisma Wishlist model to a simplified format.
    - Added fields like id, name, userId, createdAt, and updatedAt.

--- shared/types/Wishlist.ts
+++ shared/types/Wishlist.ts
@@ -1,7 +1,19 @@
-export type Wishlist = {
-    name: string             
-    userId: string 
-    propertyId: string
+import { Wishlist as PrismaWishlist } from '@prisma/client';
+
+export type Wishlist = {                    
+    id: string
+    name: string
+    userId: string
     createdAt: Date
     updatedAt: Date
+}
+
+export const WishlistDTO = (wishlist: PrismaWishlist): Wishlist => {
+    return {
+        id: wishlist.id,
+        name: wishlist.name,
+        userId: wishlist.userId,
+        createdAt: wishlist.createdAt,
+        updatedAt: wishlist.updatedAt
+    }
 }
