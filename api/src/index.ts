5433f793ecd2acee19da319dba231aab03a0f10f
Joey Bervin <bervin.joey@hotmail.com>
2024-12-18 08:09:17 +0100

    feat(docs): partial Swagger setup with login and register routes
    - Added Swagger configuration to document API routes.
    - Documented `/auth/login` and `/auth/register` endpoints.
    - Defined reusable `UserRequest` and `UserResponse` schemas under `components.schemas`.
    - Included Swagger UI accessible at `/api-docs`.
    
    Note: Swagger documentation is partially complete. Additional routes and schemas will be added in future updates.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response, Application } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './configs/swaggerConfig'; 
 import router from "./routes/routes";
+
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -14,12 +17,18 @@ const app: Application = express();
 // app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morganMiddleware)
+app.use(morganMiddleware);
+
 
 /* ROUTER / ROUTES */
-app.use(router)
 
-app.use(errorHandler)
+app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use(errorHandler);
+
+// console.log(JSON.stringify(swaggerSpec, null, 2));
+
 
 app.use("*", (req: Request, res: Response) => {
     res.status(404).json({
@@ -32,5 +41,6 @@ app.use("*", (req: Request, res: Response) => {
 
 const port = process.env.API_PORT || 3001;
 app.listen(port, () => {
-    serverLogger.info(`App listening on port  http://localhost:${port}`)
-})  
+    serverLogger.info(`App listening on port  http://localhost:${port}`);
+    serverLogger.info(`Swagger docs available at http://localhost:${port}/api-docs`);
+});
--- api/src/routes/home.routes.ts
+++ api/src/routes/home.routes.ts
@@ -3,6 +3,15 @@ import { HomeController } from '../controllers/home.controller';
 
 const router = Router();
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Ping the server
+ *     responses:
+ *       200:
+ *         description: Pong!
+ */
 router.get('/', HomeController.getHome);
 
 export default router;
