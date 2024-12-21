f8e7b24ad91618259f46ac1dad35d671f53adca0
Joey Bervin <bervin.joey@hotmail.com>
2024-12-21 20:46:38 +0100

    fix2(.env0): add deleted .env file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const apiUrl = process.env.API_URL;
-
+const apiUrl: string | undefined = process.env.VITE_ENV === "development" ? "http://localhost:3000" : process.env.API_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': `${apiUrl}`,
+      '/api-etnair': `${apiUrl}`,
     },
   },
 })
